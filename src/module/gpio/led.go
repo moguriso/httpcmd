cdbea626649ea5e68140fef3bb8af577a308d3cf
Yuya Adachi <moguriso@tizen.moe>
2018-01-18 00:34:44 +0900

    LED色変え手前でONするように修正

--- src/module/gpio/led.go
+++ src/module/gpio/led.go
@@ -34,33 +34,41 @@ func LedOff() {
 }
 
 func LedBlue() {
+	ledCtrl(4)
 	ledCtrl(5)
 }
 
 func LedGreen() {
+	ledCtrl(4)
 	ledCtrl(18)
 }
 
 func LedLime() {
+	ledCtrl(4)
 	ledCtrl(13)
 }
 
 func LedPerple() {
+	ledCtrl(4)
 	ledCtrl(12)
 }
 
 func LedPink() {
+	ledCtrl(4)
 	ledCtrl(22)
 }
 
 func LedRed() {
+	ledCtrl(4)
 	ledCtrl(27)
 }
 
 func LedWhite() {
+	ledCtrl(4)
 	ledCtrl(6)
 }
 
 func LedYellow() {
+	ledCtrl(4)
 	ledCtrl(23)
 }
