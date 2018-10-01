1081e2e345f7cd8b640dca00ab4280f5eeb8a141
Yuya Adachi <moguriso@tizen.moe>
2018-10-01 22:53:29 +0900

    add ir commands and ...

---
+++ src/module/httpcmd/timerloop.go
@@ -0,0 +1,17 @@
+package httpcmd
+
+//func timerInit() {
+//}
+//
+//func TimerLoop() int {
+//	timerInit()
+//	t := time.NewTicker(time.Duration(timerInterval) * time.Millisecond)
+//	defer t.Stop()
+//	for {
+//		select {
+//		case <-t.C:
+//			logr.Debug("Timer function in")
+//		}
+//	}
+//	return 0
+//}
